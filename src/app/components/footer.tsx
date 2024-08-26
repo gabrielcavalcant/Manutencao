699e35a0a738f395683c105b3aaaa812e0de5f1d
gabrielcavalcant <gabriel_sep@outlook.com>
2024-08-26 16:06:50 -0300

    fix: Correct typos and update text in footer
    Corrected spelling mistakes and updated text content in the footer component for better readability.

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <div className="w-full bg-background p-4">
       <footer className="text-center text-foreground text-sm">
-        Todos os direitos reservados &copy; 2024
+        Todos os direitos reservados ao meu Verdão, o maior do BRASIL! &copy; 2024
       </footer>
     </div>
   );
